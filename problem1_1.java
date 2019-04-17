45b9bf96d7d468499e32b3968c660e606b06df62
Mizuho Yamaguchi <s1250145u@gmail.com>
2019-04-17 11:18:26 +0900

    update java file

--- problem1_1.java
+++ problem1_1.java
@@ -30,13 +30,13 @@ public class problem1_1 {
   }
 
   public void compute() {
-    int y1 = listN[listM[0][1]-1][1] - listN[listM[0][0]-1][1];
-    int x2 = listN[listM[1][1]-1][0] - listN[listM[1][0]-1][0];
-    int x1 = listN[listM[0][1]-1][0] - listN[listM[0][0]-1][0];
-    int y2 = listN[listM[1][0]-1][1] - listN[listM[1][1]-1][1];
+    double y1 = listN[listM[0][1]-1][1] - listN[listM[0][0]-1][1];
+    double x2 = listN[listM[1][1]-1][0] - listN[listM[1][0]-1][0];
+    double x1 = listN[listM[0][1]-1][0] - listN[listM[0][0]-1][0];
+    double y2 = listN[listM[1][0]-1][1] - listN[listM[1][1]-1][1];
+    double formulaA = Math.abs( ((x1) * (y2)) + ((x2) * (y1)) );
 
-    int formulaA = Math.abs( (x1) * (y2) + (x2) * (y2) );
-    double s,t;
+    double s, t, xp, yp;
 
     // Step1
     if(formulaA == 0){
@@ -45,11 +45,11 @@ public class problem1_1 {
       return;
     } else {
       // Step2
-      int xp, yp;
       xp = listN[listM[1][0]-1][0] - listN[listM[0][0]-1][0];
       yp = listN[listM[1][0]-1][1] - listN[listM[0][0]-1][1];
-      s = (y2*xp + x2*yp) / formulaA;
-      t = (y1*xp + x1*yp) / formulaA;
+      double yPq = listN[listM[0][0]-1][1] - listN[listM[0][1]-1][1];
+      s = (y2*xp + x2*yp) * 1.0 / formulaA;
+      t = (yPq*xp + x1*yp) * 1.0 / formulaA;
     }
 
     // Step3
@@ -57,7 +57,14 @@ public class problem1_1 {
       // Step4
       answerX = listN[listM[0][0]-1][0] + x1 * s;
       answerY = listN[listM[0][0]-1][1] + y1 * s;
-      output(answerX, answerY);
+      for(int i=0; i<N; i++) {
+        if(answerX == listN[i][0] && answerY == listN[i][1]){
+          output("NA");
+          return;
+        } else {
+          output(answerX, answerY);
+        }
+      }
     } else {
       // 交差なし
       output("NA");
