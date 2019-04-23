56675724d40dd6db5cb60722a9bad6efe4194e22
Mizuho Yamaguchi <s1250145u@gmail.com>
2019-04-24 07:54:18 +0900

    problem1_2.javaの追加

---
+++ problem1_2.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class problem1_2 extends problem1_1 {
+
+  public problem1_2() {
+  }
+
+  public void sort() {
+
+  }
+
+  public void output() {
+  }
+
+  public static void main(String[] args) {
+    problem1_2 obj = new problem1_2();
+    obj.output();
+  }
+}
