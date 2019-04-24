31c922ef2015d68198059c1e3272b910641b1be0
Mizuho Yamaguchi <s1250145u@gmail.com>
2019-04-24 12:17:53 +0900

    problem1_1.javaのリファクタリング

--- problem1_1.java
+++ problem1_1.java
@@ -23,6 +23,7 @@ public class problem1_1 {
   Scanner sc = new Scanner(System.in);
   int N,M,P,Q;
   double answerX, answerY;
+  double x1, x2, y1, y2;
 
   N[] nList;
   M[] roads;
@@ -53,10 +54,10 @@ public class problem1_1 {
   public void compute() {
     for(int i=0; i<M; i++) {
       for(int j=i+1; j<M; j++) {
-        double y1 = nList[roads[i].q].y - nList[roads[i].p].y;
-        double x2 = nList[roads[j].q].x - nList[roads[j].p].x;
-        double x1 = nList[roads[i].q].x - nList[roads[i].p].x;
-        double y2 = nList[roads[j].p].y - nList[roads[j].q].y;
+        y1 = nList[roads[i].q].y - nList[roads[i].p].y;
+        x2 = nList[roads[j].q].x - nList[roads[j].p].x;
+        x1 = nList[roads[i].q].x - nList[roads[i].p].x;
+        y2 = nList[roads[j].p].y - nList[roads[j].q].y;
         double formula = Math.abs(((x1) * (y2)) + ((x2) * (y1)));
         double s, t, xp, yp, yPq;
 
@@ -66,17 +67,17 @@ public class problem1_1 {
           yPq = nList[roads[i].p].y - nList[roads[i].q].y;
           s = (y2*xp + x2*yp) * 1.0 / formula;
           t = (yPq*xp + x1*yp) * 1.0 / formula;
-        } else {
-          continue;
-        }
-
-        if((s>0.0 && s<1.0) && (t>0.0 && t<1.0)) {
-          answerX = nList[roads[i].p].x + x1 * s;
-          answerY = nList[roads[i].p].y + y1 * s;
-          Answer point = new Answer(answerX, answerY);
-          answer.add(point);
+          recordAnswer(i, s, t);
         }
       }
+    }
+  }
+
+  public void recordAnswer(int i, double s, double t) {
+    if((s>0.0 && s<1.0) && (t>0.0 && t<1.0)) {
+      answerX = nList[roads[i].p].x + x1 * s;
+      answerY = nList[roads[i].p].y + y1 * s;
+      answer.add(new Answer(answerX, answerY));
     }
   }
 
